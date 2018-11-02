9be1964b643ef8b65807def8218e8bb1fa20818a
TigrexR <swcdasd@sina.com>
2018-11-02 14:58:16 +0800

    all

---
+++ src/test/java/com/tigrex/geo/KnowledgeTest.java
@@ -0,0 +1,89 @@
+package com.tigrex.geo;
+
+import com.tigrex.geo.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowledgeTest {
+
+    @Test
+    public void stringTest(){
+        //stringbuffer
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("hello world");
+        stringBuffer.append("---bye world");
+        System.out.println(stringBuffer.length());
+        System.out.println(stringBuffer.toString());
+        String subStr = stringBuffer.substring(1, 4);
+        System.out.println(subStr);
+        stringBuffer.delete(1,4);
+        System.out.println(stringBuffer.toString());
+
+        //stringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("hello world");
+        stringBuilder.substring(1,4);
+
+        String s = new String();
+    }
+
+    @Test
+    public void boxTest(){
+
+        Integer num1 = 1;
+        int num2 = 1;
+        System.out.println(num1 == num2);
+
+        Integer num3 = 1234;
+        int num4 = 1234;
+        System.out.println(num3.intValue());
+        System.out.println(num3 == num4);
+
+        Set<Integer> integerSet = new TreeSet<>();
+        System.out.println(integerSet.getClass());
+
+        List<Integer> integerList = List.of(1, 3);
+        System.out.println(integerList.getClass());
+
+        Queue<Integer> integerQueue = new ArrayDeque<>();
+        integerQueue.add(1);
+        integerQueue.add(2);
+        integerQueue.add(3);
+        System.out.println(integerQueue.poll());
+        System.out.println(integerQueue.toString());
+
+    }
+
+    @Test
+    public void feflectionTest(){
+        try {
+            Class clazz = Class.forName("com.tigrex.geo.domain.User");
+            System.out.println(clazz.getName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void strongReferenceTest(){
+        //强引用
+        User george = new User().setId(1).setName("george").setAge(13);
+        System.out.println(george.toString());
+    }
+
+    @Test
+    public void vectorTest(){
+        List<Integer> integers = new Vector();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        System.out.println(integers.toString());
+    }
+
+}
